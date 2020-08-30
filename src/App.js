1030d41d5b7176d304406e1ce2a0c8f400c2377b
aledeloss <aledeloss@gmail.com>
2020-08-30 09:00:53 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import './App.css';
+
+
+function App() { 
+
+const [estaSonando, setEstaSonando] = useState("");
+
+const notas = [
+  {
+    nombre: "Do",
+    link: require("./notes/do.wav"),
+    tieneSostenido: true,
+    tecla: "d"
+  },
+  {
+    nombre: "Re",
+    link: require("./notes/re.wav"),
+    tieneSostenido: true,
+    tecla: "f"
+  },
+  {
+    nombre: "Mi",
+    link: require("./notes/mi.wav"),
+    tecla: "g"
+  },
+  {
+    nombre: "Fa",
+    link: require("./notes/fa.wav"),
+    tieneSostenido: true,
+    tecla: "h"
+  },
+  {
+    nombre: "Sol",
+    link: require("./notes/sol.wav"),
+    tieneSostenido: true,
+    tecla: "i"
+  },
+  {
+    nombre: "La",
+    link: require("./notes/la.wav"),
+    tieneSostenido: true,
+    tecla: "j"
+  },
+  {
+    nombre: "Si",
+    link: require("./notes/si.wav"),
+    tecla: "k"
+  },
+]
+
+const handleClick = (nota) => {
+  setEstaSonando(nota.nombre);
+  const sonido = new Audio(nota.link);
+  sonido.play();
+  console.log(estaSonando);
+
+  setTimeout(() => {
+    setEstaSonando("");
+  }, 300)
+}
+
+  return (
+    <div className="App">
+      <div className="titulo">
+        <h1>Piano con react</h1>
+        <h2>Hecho con sueño por Ale</h2>
+          { estaSonando &&
+            <h3>Está sonando la nota {estaSonando}</h3>
+          }
+      </div>
+
+      <div className="contenedor"></div>
+      {
+        notas.map(nota => {
+          return(
+            <div className={`nota ${estaSonando === nota.nombre && 'estaSonando'}`}
+            onClick={()=>handleClick(nota) }>
+              { nota.tieneSostenido && <div className="negra"></div>}
+            </div>
+          )
+        })
+      }
+    </div>
+  );
+}
+
+export default App;
