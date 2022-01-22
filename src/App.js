e5e16263ab15b8c38af1d636565c6dc27631b5d5
Aledeloss <Aledeloss@gmail.com>
2022-01-22 07:27:07 -0300

    Pruebas de refactor de proyecto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
+import { Tecla } from './components/Tecla/Tecla';
+import useTocarNota from './hooks/useTocarNota';
 import { notas } from './notas.js';
 
 function App() {
@@ -23,29 +25,22 @@ function App() {
   };
 
   const handleKeyPress = (e, nota) => {
-    if (e.code === 'KeyH') {
-      setEstaSonando(nota.nombre);
-      const sonido = new Audio(nota.link);
-      sonido.play();
-    }
-    if (melodia) {
-      setMelodia([...melodia, nota]);
-      var vistaMelodia = melodia.join(' ');
-    } else {
-      setMelodia([nota]);
-      var vistaMelodia = melodia.join(' ');
-    }
-    setTimeout(() => {
-      console.log(vistaMelodia);
-      console.log(melodia);
-    }, 1000);
-
-    setTimeout(() => {
-      setEstaSonando('');
-    }, 300);
+    // if (e.code === 'KeyH') {
+    //   setEstaSonando(nota.nombre);
+    //   const sonido = new Audio(nota.link);
+    //   sonido.play();
+    // }
+    // if (melodia) {
+    //   setMelodia([...melodia, nota]);
+    // } else {
+    //   setMelodia([nota]);
+    // }
+    // setTimeout(() => {
+    //   setEstaSonando('');
+    // }, 300);
   };
 
-  const tocarNota = async (melodia, nota) => {
+  const tocarNotita = async (melodia, nota) => {
     console.log(melodia[nota]);
     setEstaSonando(nota.nombre);
     const sonido = new Audio(melodia[nota].link);
@@ -54,7 +49,7 @@ function App() {
 
   const reproducirMelodia = async (melodia, i) => {
     const nota = i | 0;
-    await tocarNota(melodia, nota).then(() => {
+    await tocarNotita(melodia, nota).then(() => {
       if (nota < melodia.length - 1) {
         setTimeout(() => {
           reproducirMelodia(melodia, nota + 1);
@@ -69,7 +64,6 @@ function App() {
     });
     return nombresMelodia.join(' ');
   };
-  console.log(typeof renderMelodia);
 
   const borrarMelodia = () => {
     console.log('Borraste la melodía');
@@ -79,7 +73,7 @@ function App() {
   return (
     <div className='App'>
       <div className='titulo'>
-        <h1>Piano con react</h1>
+        <h1>Pianuli</h1>
         <h2>Hecho con sueño y entusiasmo por Ale</h2>
         {estaSonando && <h3>Está sonando la nota {estaSonando}</h3>}
       </div>
@@ -87,14 +81,22 @@ function App() {
       <div className='contenedor'>
         {notas.map((nota) => {
           return (
-            <div
-              className={`nota ${estaSonando === nota.nombre && 'estaSonando'}`}
-              onClick={() => handleClick(nota)}
-              onkeypress={() => handleKeyPress(nota)}
-            >
-              {nota.tieneSostenido && <div className='negra'></div>}
-              {nota.tecla}
-            </div>
+            <Tecla
+              nota={nota}
+              estaSonando={estaSonando}
+              melodia={melodia}
+              setMelodia={setMelodia}
+              handleClick={handleClick}
+              handleKeyPress={handleKeyPress}
+            />
+            // <div
+            //   className={`nota ${estaSonando === nota.nombre && 'estaSonando'}`}
+            //   onClick={() => handleClick(nota)}
+            //   onkeypress={() => handleKeyPress(nota)}
+            // >
+            //   {nota.tieneSostenido && <div className='negra'></div>}
+            //   {nota.tecla}
+            // </div>
           );
         })}
       </div>
---
+++ src/components/Tecla/Tecla.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+import '../../App.css';
+
+export const Tecla = (nota, handleClick, handleKeyPress, melodia, setMelodia) => {
+  const [estaSonando, setEstaSonando] = useState('');
+
+  const handleClick2 = (nota) => {
+    console.log(nota);
+    setEstaSonando(nota.nombre);
+    const sonido = new Audio(nota.link);
+    sonido.play();
+
+    if (melodia) {
+      setMelodia([...melodia, nota]);
+    } else {
+      setMelodia([nota]);
+    }
+
+    setTimeout(() => {
+      setEstaSonando('');
+    }, 300);
+  };
+  return (
+    <div
+      className={`nota ${estaSonando === nota.nombre && 'estaSonando'}`}
+      onClick={() => handleClick2(nota)}
+      onkeypress={() => handleKeyPress(nota)}
+    >
+      {nota.tieneSostenido && <div className='negra'></div>}
+      {nota.tecla}
+    </div>
+  );
+};
